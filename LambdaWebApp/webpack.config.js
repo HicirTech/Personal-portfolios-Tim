ce20424976686ae9fa9d6bfef0118904583eaf8f
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2020-10-06 10:57:06 +1300

    simple webpack config

--- LambdaWebApp/webpack.config.js
+++ LambdaWebApp/webpack.config.js
@@ -1,23 +1,25 @@
 const path = require("path");
-module.exports = {
-  entry: {
-    main: "./src/index.jsx",
-  },
-  output: {
-    filename: "[name].js",
-    globalObject: "this",
-    path: path.resolve(__dirname, "wwwroot/dist"),
-    publicPath: "dist/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = (env) => {
+  return {
+    entry: {
+      main: "./src/index.jsx",
+    },
+    output: {
+      filename: "[name].js",
+      globalObject: "this",
+      path: path.resolve(__dirname, "wwwroot/dist"),
+      publicPath: "dist/",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
         },
-      },
-    ],
-  },
+      ],
+    },
+  };
 };
