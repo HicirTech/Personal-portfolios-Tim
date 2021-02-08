b7c51ef107d16f1f72a2e8aea144f94803b380bb
电子腋毛程序/Electronic hirci program <luozeting97@gmail.com>
2021-02-09 12:53:10 +1300

    Basic connection for signalR

---
+++ ServerBackend/Hubs/LoginChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+
+namespace ServerBackend.Hubs
+{
+    public class LoginChecker : Hub
+    {
+        private static int counter = 0;
+        public LoginChecker()
+        {
+
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            counter++;
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? e)
+        {
+            //custom logic here
+            counter--;            
+            return base.OnDisconnectedAsync(e);
+        }
+
+        public async Task onLoad()
+        {
+
+            await Clients.Caller.SendAsync("onLoadConfirm", counter);
+
+        }
+
+
+    }
+}
